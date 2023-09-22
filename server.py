44db2c67dc81ed235f77605b88b26cc2f56a5633
divyansh <dvynsh24@gmail.com>
2023-09-22 16:57:35 +0530

    added the server

---
+++ server.py
@@ -0,0 +1,43 @@
+from socket import gethostbyname, gethostname, socket, AF_INET, SOCK_STREAM
+
+import threading
+
+
+SERVER_IP = gethostbyname(gethostname())
+PORT = 6969
+
+ADDRESS = (SERVER_IP, PORT)
+
+server = socket(AF_INET, SOCK_STREAM)
+# IPV4 and TCP based server
+
+
+with server as s:
+    s.bind(ADDRESS)
+
+    s.listen()
+    # enables a server to accept connections. It makes the server a “listening” socket
+    # NOTE: can take a param int "backlog"
+    # specifies the number of unaccepted connections that the system will allow before
+    # refusing new connections
+
+    print(f"Listening at {SERVER_IP}:{PORT}")
+
+    connection, address_info = s.accept()
+    # blocks execution and waits for an incoming connection
+    # client connects, it returns a new socket object
+    # with connection and the address of the client
+    # SIDENOTE: (host, port, flowinfo, scopeid) for IPv6
+
+    with connection:
+        print(f"Connected to {address_info}")
+
+        while True:
+            # loop over blocking calls to conn.recv()
+            data = connection.recv(1024)
+            # reads whatever data the client sends and echoes back using conn.sendall()
+
+            if not data:
+                break
+
+            connection.sendall(data)
