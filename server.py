ea6fda48901146ff1c5ce1cbb3ab6cdffe728e3e
dpi0 <dpi0.dev@proton.me>
2023-11-14 19:26:26 +0530

    updated both

--- server.py
+++ server.py
@@ -1,4 +1,11 @@
-from socket import gethostbyname, gethostname, socket, AF_INET, SOCK_STREAM
+from socket import (
+    gethostbyname,
+    gethostname,
+    socket,
+    AF_INET,
+    SOCK_STREAM,
+    error,
+)
 
 import threading
 
@@ -8,36 +15,45 @@ PORT_USED = 6969
 
 ADDRESS = (SERVER_IP, PORT_USED)
 
-server = socket(AF_INET, SOCK_STREAM)
-# IPV4 and TCP based server
+try:
+    server = socket(AF_INET, SOCK_STREAM)
+    # IPV4 and TCP based server
 
+    with server:
+        server.bind(ADDRESS)
 
-with server:
-    server.bind(ADDRESS)
+        server.listen()
+        # enables a server to accept connections.
+        # It makes the server a “listening” socket
+        # NOTE: can take a param int "backlog"
+        # specifies the number of unaccepted connections
+        # that the system will allow before
+        # refusing new connections
 
-    server.listen()
-    # enables a server to accept connections. It makes the server a “listening” socket
-    # NOTE: can take a param int "backlog"
-    # specifies the number of unaccepted connections that the system will allow before
-    # refusing new connections
+        print(f"Listening at {SERVER_IP}:{PORT_USED}...")
 
-    print(f"Listening at {SERVER_IP}:{PORT_USED}")
+        connection, address_info = server.accept()
+        # blocks execution and waits for an incoming connection
+        # client connects, it returns a new socket object
+        # with connection and the address of the client
+        # SIDENOTE: (host, port, flowinfo, scopeid) for IPv6
 
-    connection, address_info = server.accept()
-    # blocks execution and waits for an incoming connection
-    # client connects, it returns a new socket object
-    # with connection and the address of the client
-    # SIDENOTE: (host, port, flowinfo, scopeid) for IPv6
+        with connection:
+            print(f"Connected to {address_info}")
+            # connection.send(
+            #     f"You are now connected to {SERVER_IP}:{PORT_USED}".encode()
+            # )
 
-    with connection:
-        print(f"Connected to {address_info}")
+            while True:
+                # loop over blocking calls to conn.recv()
+                data = connection.recv(1024)
+                # reads whatever data the client sends
+                # and echoes back using conn.sendall()
 
-        while True:
-            # loop over blocking calls to conn.recv()
-            data = connection.recv(1024)
-            # reads whatever data the client sends and echoes back using conn.sendall()
+                if not data:
+                    break
 
-            if not data:
-                break
+                connection.sendall(data)
 
-            connection.sendall(data)
+except error as socket_error:
+    print("Socket Creation Failed...", socket_error)
